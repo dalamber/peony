24dfa38fa475719081020c40dc631f4126337b1c
Anton Shpakovskiy <saa@tomsksoft.com>
2016-06-18 21:46:10 +0600

    Added basic project structure.

---
+++ peony/peony-common/src/helpers/foo.cpp
@@ -0,0 +1,5 @@
+#include "foo.h"
+
+int common::helpers::getVal() {
+    return 42;
+}
---
+++ peony/peony-common/src/helpers/foo.h
@@ -0,0 +1,12 @@
+#ifndef PEONY_COMMON_HELPERS_FOO_H
+#define PEONY_COMMON_HELPERS_FOO_H
+
+namespace common
+{
+    namespace helpers
+    {
+        int getVal();
+    }
+}
+
+#endif //PEONY_COMMON_HELPERS_FOO_H
